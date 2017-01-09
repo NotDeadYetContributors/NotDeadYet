706d531454a30d55e51ad817d22cc4fe9ebeba88
Jaxel A Rojas <jaxelr@gmail.com>
2017-01-09 00:02:09 -0400

    Implemented NotDeadYet.ServiceStack for Service Stack version 4 and up. Added Sample webservice solution testing the NotDeadYet.ServiceStack library.

---
+++ src/NotDeadYet.Samples.ServiceStack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NotDeadYet.Samples.ServiceStack.Global" Language="C#" %>
---
+++ src/NotDeadYet.ServiceStack/HealthCheckService.cs
@@ -0,0 +1,32 @@
+﻿using NotDeadYet.Results;
+using ServiceStack;
+using System.Net;
+
+namespace NotDeadYet.ServiceStack
+{
+    public class HealthCheckService : Service
+    {
+        private readonly IHealthChecker _healthChecker;
+
+        public HealthCheckService(IHealthChecker healthChecker)
+        {
+            _healthChecker = healthChecker;
+        }
+
+        public object Get(HealthCheck request)
+        {
+            var result = _healthChecker.Check();
+            var statusCode = (result.Status == HealthCheckStatus.NotOkay)
+                ? HttpStatusCode.ServiceUnavailable
+                : HttpStatusCode.OK;
+
+            return new HttpResult(result, statusCode);
+        }
+    }
+
+    [Api("Health Check")]
+    [Route("/healthcheck", Verbs = "GET", Summary = "Checks if the website is online", Notes = "Health Check")]
+    public class HealthCheck
+    {
+    }
+}
