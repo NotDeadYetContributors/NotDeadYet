edbe8c0c9facd9494b497e91531f837608e97a62
Tristan Menzel <tristan.menzel@readify.net>
2017-03-30 09:22:20 +1000

    Add OwinMiddleware for serving the health check end point.

---
+++ src/NotDeadYet.Samples.Owin/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using NotDeadYet.Owin;
+
+[assembly: OwinStartup(typeof(NotDeadYet.Samples.Owin.Startup))]
+
+namespace NotDeadYet.Samples.Owin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureHealthCheck(app);
+        }
+
+        private void ConfigureHealthCheck(IAppBuilder app)
+        {
+            var healthChecker = new HealthCheckerBuilder()
+                .WithHealthChecksFromAssemblies(typeof(Startup).Assembly)
+                .Build();
+
+            app.UseHealthCheck(healthChecker);
+        }
+    }
+}
